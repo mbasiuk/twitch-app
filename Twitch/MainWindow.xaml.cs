53ca83c25025c98ef7be93b2a5b4994a594819eb
mike <mykhailo.basiuk@gmail.com>
2017-01-04 17:55:36 +0200

    Twitch App;

--- Twitch/MainWindow.xaml.cs
+++ Twitch/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Twitch
 {
@@ -22,7 +9,16 @@ namespace Twitch
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();                       
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource streamViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("streamViewSource")));
+            streamViewSource.Source = Models.Loaders.Dota2.Get().streams;
+            // Load data by setting the CollectionViewSource.Source property:
+            // streamViewSource.Source = [generic data source]
         }
     }
 }
---
+++ Twitch/Models/Channels.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Twitch.Settings;
+
+namespace Twitch.Models
+{
+
+    public class Channel
+    {
+        public string name { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Stream
+    {
+        public Channel channel { get; set; }
+        public int viewers { get; set; }
+        public Preview preview { get; set; }
+    }
+
+    public class Preview
+    {
+        public string small { get; set; }
+        public string medium { get; set; }
+        public string large { get; set; }
+        public string template { get; set; }
+    }
+
+    public class StreamCollection
+    {
+        public List<Stream> streams { get; set; }
+    }
+}
+
+namespace Twitch.Settings
+{
+    public class Auth
+    {
+        public const string ClientIdHeader = "Client-ID";
+        public const string ClientId = "7bjbjzp3jzq3zc2n55epfoih9s54zn";
+    }
+}
+
+namespace Twitch.Models.Loaders
+{
+    public class Dota2
+    {
+        const string ApiUrl = "https://api.twitch.tv/kraken/streams?limit=20&offset=0&game=Dota+2&on_site=1";
+        public static StreamCollection Get()
+        {
+            using (var client = new System.Net.WebClient())
+            {
+                client.Headers.Add(Auth.ClientIdHeader, Auth.ClientId);
+                client.Encoding = System.Text.Encoding.UTF8;
+                string response = client.DownloadString(ApiUrl);
+                var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                StreamCollection gameJSON = serializer.Deserialize<StreamCollection>(response);
+                return gameJSON;
+            }
+            throw new System.NotImplementedException();
+        }
+
+        public async static Task<StreamCollection> GetAsync()
+        {
+            using (var client = new System.Net.WebClient())
+            {
+                client.Headers.Add(Auth.ClientIdHeader, Auth.ClientId);
+                client.Encoding = System.Text.Encoding.UTF8;
+                string response = await client.DownloadStringTaskAsync(ApiUrl);
+                var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                StreamCollection gameJSON = serializer.Deserialize<StreamCollection>(response);
+                return gameJSON;
+            }
+        }
+    }
+}
+
+
