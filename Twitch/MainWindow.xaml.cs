8745d8b00a250a8099b40fb670d888fcae02dea9
mike <mykhailo.basiuk@gmail.com>
2017-01-05 13:36:09 +0200

    Improve Startup UI experience

--- Twitch/MainWindow.xaml.cs
+++ Twitch/MainWindow.xaml.cs
@@ -12,11 +12,11 @@ namespace Twitch
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             System.Windows.Data.CollectionViewSource streamViewSource = ((System.Windows.Data.CollectionViewSource)(FindResource("streamViewSource")));
             var loader = new Models.Loaders.Dota2();
-            var streamCollection = loader.Get();
+            var streamCollection = await loader.GetAsync();
                         
             streamViewSource.Source = streamCollection.streams;
             loader.UpdateEvery(streamCollection, Settings.StreamsRefresher.IntervalInSeconds, streamViewSource.View);            
