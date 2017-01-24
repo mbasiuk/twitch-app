07a35f0ac166ded7719ec66e4dff7fea74bb391f
mike <mykhailo.basiuk@gmail.com>
2017-01-24 22:13:48 +0200

    twitch api update. api key is personal;

--- Open-Stream.ps1
+++ Open-Stream.ps1
@@ -1,2 +1,2 @@
 param([string]$chennel="starladder1")
-& 'C:\Program Files (x86)\Livestreamer\livestreamer.exe' twitch.tv/$chennel source --player "'C:\Program Files (x86)\MPC-HC\mpc-hc.exe' /new"
+& 'C:\Program Files (x86)\Livestreamer\livestreamer.exe' twitch.tv/$chennel source --player "'C:\Program Files (x86)\MPC-HC\mpc-hc.exe' /new" --http-header Client-ID=7bjbjzp3jzq3zc2n55epfoih9s54zn
